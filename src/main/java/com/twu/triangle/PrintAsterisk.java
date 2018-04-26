bb89bb9d15bc85517cbe504d931eeae9fc6c93c4
LisaHuang <1449700087@qq.com>
2018-04-26 11:23:05 +0800

    PrintAsterisk.java: Print one asterisk to the console.

---
+++ src/main/java/com/twu/triangle/PrintAsterisk.java
@@ -0,0 +1,10 @@
+package com.twu.triangle;
+
+/**
+ * Created by ASUS on 2018/4/26.
+ */
+public class PrintAsterisk {
+    public static void main(String[] args) {
+        System.out.println("*");
+    }
+}
