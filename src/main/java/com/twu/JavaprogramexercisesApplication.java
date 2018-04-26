46d454a2de64397eb2e9bc1d76aaea8ce35425ad
LisaHuang <1449700087@qq.com>
2018-04-26 11:19:50 +0800

    project init

---
+++ src/main/java/com/twu/JavaprogramexercisesApplication.java
@@ -0,0 +1,12 @@
+package com.twu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaprogramexercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaprogramexercisesApplication.class, args);
+	}
+}
